3fad823c3d30c6a092aca403097c7639403f194b
Chandan168 <chandsharma@algomau.ca>
2025-05-31 02:02:13 +0000

    Created config.php with DB credentials

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$host = "dpblo.h.filess.io";        // Hardcoded
+$port = "61000";                // Hardcoded
+$dbname = "";      // Hardcoded
+$dbname = "Cosc4806_modelbirth";      // Hardcoded
+$username = "Cosc4806_modelbirth";// Hardcoded
+$password = getenv('DB_PASS'); // Secret password 
+?>
